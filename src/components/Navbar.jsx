463792ab6953b34b20ab1d51d99704d033da7a5b
Abeed <abeedabeed437@gmail.com>
2025-12-21 13:15:08 +0530

    fixed most bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
-import React ,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 function Navbar() {
     const [menuOpen,setMenuOpen] = useState(false);
+    const location = useLocation();
+
+    // Close menu on route change (ensures links close menu on navigation)
+    useEffect(() => {
+      setMenuOpen(false);
+    }, [location]);
+
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
+
+    const closeMenu = () => setMenuOpen(false);
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">MovieFinder</div>
             <ul className = {`navbar-links ${menuOpen ? "active" : ""}`}>
-                <li><Link to = "/">Home</Link></li>
-                <li><Link to = "/favorites">Favorites</Link></li>
-                <li><Link to = "">Contact us</Link></li>
-                <li><Link to = "">blog</Link></li>
+                <li><Link to = "/" onClick={closeMenu}>Home</Link></li>
+                <li><Link to = "/favorites" onClick={closeMenu}>Favorites</Link></li>
+                <li><Link to = "" onClick={closeMenu}>Contact us</Link></li>
+                <li><Link to = "" onClick={closeMenu}>blog</Link></li>
             </ul>
             <div className="hamburger" onClick = {toggleMenu}>
                {menuOpen ? "X" : "☰"}
